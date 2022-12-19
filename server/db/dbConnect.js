b5b1f865b1e77bdd3359d5bec52cf25b66b8c107
vadiraj03 <vadiraj.kanakagiri@stenum-asia.org>
2022-12-19 13:13:07 +0530

    preparing to deploy

--- server/db/dbConnect.js
+++ server/db/dbConnect.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
+require('dotenv').config();
 
 const con = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'kfi',
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DATABASE,
 });
 
 module.exports = con;
