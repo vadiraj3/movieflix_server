1b3dd1705ed9e797f65bc339bfd000c0d83ccc0d
vadiraj3 <71882586+vadiraj3@users.noreply.github.com>
2023-07-20 13:46:28 +0530

    Update dbConnect.js

--- server/db/dbConnect.js
+++ server/db/dbConnect.js
@@ -1,12 +1,14 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 require('dotenv').config();
 
-const con = mysql.createConnection({
+const con = mysql.createPool({
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_DATABASE,
 	port: process.env.DB_PORT,
+	connectionLimit: 10, // Maximum number of connections in the pool
+	waitForConnections: true, // Wait for a free connection if no connections are available
 });
 
 module.exports = con;
