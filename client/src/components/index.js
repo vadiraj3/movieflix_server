d422880720cb53a4eb614dd97a433353d9fac013
vadiraj3 <71882586+vadiraj3@users.noreply.github.com>
2023-07-20 11:23:24 +0530

    Delete client directory

--- client/src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Actors } from './Actors/Actors';
-export { default as MovieInformation } from './MovieInformation/MovieInformation';
-export { default as Movies } from './Movies/Movies';
-export { default as Navbar } from './Navbar/Navbar';
-export { default as Profile } from './Profile/Profile';
-export { default as MovieList } from './MovieList/MovieList';
-export { default as Search } from './Search/Search';
